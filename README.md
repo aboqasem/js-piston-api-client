7a4a4caee70d4769fc45e53715768910b25fbfac
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-04-12 17:50:00 +0800

    docs: update docs

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
 # Piston API Client
 
-Client wrapper for the Piston Code Execution Engine API
+Client wrapper for the [Piston Code Execution Engine](https://github.com/engineer-man/piston) API.
+
+Inspired by [node-piston](https://github.com/dthree/node-piston).
+
+## Installation
+
+```bash
+npm i piston-api-client
+yarn add piston-api-client
+pnpm add piston-api-client
+```
+
+## Usage Example
+
+```js
+import { PistonClient } from 'piston-api-client';
+// or for node.js
+import { NodePistonClient as PistonClient } from 'piston-api-client';
+
+(async () => {
+  const pistonClient = new PistonClient({ server: 'https://emkc.org' /* default */ });
+
+  const runtimes = await pistonClient.runtimes();
+  if (runtimes.success) {
+    console.log(runtimes.data);
+    // [
+    //   {
+    //     "language": "typescript",
+    //     "version": "1.7.5",
+    //     "aliases": [
+    //       "deno-ts",
+    //       "deno"
+    //     ],
+    //     "runtime": "deno"
+    //   },
+    //   ...
+    // ]
+  }
+
+  const result = await pistonClient.execute({
+    language: 'node-js',
+    version: '*',
+    files: [
+      {
+        content: 'console.log(process.argv.slice(2))',
+      },
+    ],
+    args: ['Hello', 'World'],
+  });
+  if (result.success) {
+    console.log(result.data);
+    // {
+    //  "run": {
+    //   "stdout": "[ 'Hello', 'World' ]\n",
+    //   "stderr": "",
+    //   "code": 0,
+    //   "signal": null,
+    //   "output": "[ 'Hello', 'World' ]\n"
+    //  },
+    //  "language": "javascript",
+    //  "version": "16.3.0"
+    // }
+  }
+})();
+```
+
+### Error handling
+
+If an error occurs, the `success` property will be `false` and the `error` property will contain the error. Nothing will be thrown.
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -27,6 +27,38 @@ abstract class AbstractPistonClient {
     options: { headers: Record<string, string> },
   ): Promise<T>;
 
+  /**
+   * Get supported runtimes.
+   *
+   * If `success` is `true`, `data` will be the supported languages along with their current version and aliases. Otherwise, `error` will be the error.
+   *
+   * ## Example
+   * ```js
+   * import { PistonClient } from 'piston-api-client';
+   * // or for node.js
+   * import { NodePistonClient as PistonClient } from 'piston-api-client';
+   *
+   * (async () => {
+   *   const pistonClient = new PistonClient();
+   *   const runtimes = await pistonClient.runtimes();
+   *   if (runtimes.success) {
+   *     console.log(runtimes.data);
+   *     // [
+   *     //   {
+   *     //     "language": "typescript",
+   *     //     "version": "1.7.5",
+   *     //     "aliases": [
+   *     //       "deno-ts",
+   *     //       "deno"
+   *     //     ],
+   *     //     "runtime": "deno"
+   *     //   },
+   *     //   ...
+   *     // ]
+   *   }
+   * })();
+   * ```
+   */
   public async runtimes(): Promise<
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     { success: true; data: PistonRuntime[] } | { success: false; error: any }
@@ -47,6 +79,51 @@ abstract class AbstractPistonClient {
       });
   }
 
+  /**
+   * Execute arbitrary code.
+   *
+   * If `success` is `true`, `data` will contain 1 or 2 keys `run` and `compile`. `compile` will only be present if the language requested requires a compile stage.
+   *
+   * Each of these keys has an identical structure, containing both a stdout and stderr key, which is a string containing the text outputted during the stage into each buffer. It also contains the code and signal which was returned from each process.
+   *
+   * Otherwise if `success` is `false`, `error` will be the error.
+   *
+   * ## Example
+   * ```js
+   * import { PistonClient } from 'piston-api-client';
+   * // or for node.js
+   * import { NodePistonClient as PistonClient } from 'piston-api-client';
+   *
+   * (async () => {
+   *   const pistonClient = new PistonClient();
+   *
+   *   const result = await pistonClient.execute({
+   *     language: 'node-js',
+   *     version: '*',
+   *     files: [
+   *       {
+   *         content: 'console.log(process.argv.slice(2))',
+   *       },
+   *     ],
+   *     args: ['Hello', 'World'],
+   *   });
+   *   if (result.success) {
+   *     console.log(result.data);
+   *     // {
+   *     //  "run": {
+   *     //   "stdout": "[ 'Hello', 'World' ]\n",
+   *     //   "stderr": "",
+   *     //   "code": 0,
+   *     //   "signal": null,
+   *     //   "output": "[ 'Hello', 'World' ]\n"
+   *     //  },
+   *     //  "language": "javascript",
+   *     //  "version": "16.3.0"
+   *     // }
+   *   }
+   * })();
+   * ```
+   */
   public async execute(
     data: PistonExecuteData,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -64,7 +141,7 @@ abstract class AbstractPistonClient {
 }
 
 /**
- * Piston client for Node.js. Uses the Node.js http module to communicate with the Piston server.
+ * Piston client for Node.js. Uses the Node.js http module to communicate with the Piston API.
  *
  * Do not use this class in the browser.
  */
@@ -111,7 +188,7 @@ export class NodePistonClient extends AbstractPistonClient {
 }
 
 /**
- * Piston client for the browser or fetch API compatible environments. Uses the fetch API to communicate with the Piston server.
+ * Piston client for the browser or fetch API compatible environments. Uses the fetch API to communicate with the Piston API.
  *
  * Do not use this class in Node.js.
  */
